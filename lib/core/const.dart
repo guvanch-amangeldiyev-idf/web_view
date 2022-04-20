e8cbb1925c15dcd19f6628b9290642c5e81ee85f
guvanch-amangeldiyev-idf <guvanch.amangeldiyev@idfinance.com>
2022-04-20 09:01:56 +0300

    tested with web_view

---
+++ lib/core/const.dart
@@ -0,0 +1,19 @@
+const String kExamplePage = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Load file or HTML string example</title>
+</head>
+<body>
+
+<h1>Local demo page</h1>
+<p>
+ This is an example page used to demonstrate how to load a local file or HTML
+ string using the <a href="https://pub.dev/packages/webview_flutter">Flutter
+ webview</a> plugin.
+</p>
+
+</body>
+</html>
+''';
+// ..
